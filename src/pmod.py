7ce37a027d4d5b81d2fa04e4b7526d8c249ce757
Tyopo131 <samecob62@gmail.com>
2025-11-27 15:44:10 +0000

    Add support for $PS2

--- src/pmod.py
+++ src/pmod.py
@@ -6,19 +6,28 @@ import sys
 home: str = os.getenv("HOME")
 modules = []
 nosort_modules = []
+overwrite_module = None
+overwrite_module_2 = None
 overwrite_set = False
+overwrite_2_set = False
 for root, dirs, files in os.walk(os.path.normpath(home + "/.prompt/mods/")):
     for file in files:
         cannot_load = False
         with open(os.path.normpath(root + "/" + file), mode="r") as f:
             moddef: str = f.readline()
-            if not moddef.startswith("#?"):
-                print(f"Error loading module {file}, does not start with '#?'", file=sys.stderr)
+            if not (moddef.startswith("#?") or moddef.startswith("#>")):
+                print(f"Error loading module {file}, does not start with valid signal", file=sys.stderr)
                 print("Is it a pmod module?", file=sys.stderr, end="\n\n")
                 continue
-            moddef = moddef.removeprefix("#?")
+            PS: int = None
+            if (moddef.startswith("#>")):
+                PS = 2 # Will modify PS2
+            else:
+                moddef = moddef.removeprefix("#?")
+                PS = 1 # Will modify PS1
             priority: int = None
             remcount: int = 0
+            insert_at: int = None
             flag_manual = False
             flag_stderr = False
             flag_overwrite = False
@@ -26,12 +35,17 @@ for root, dirs, files in os.walk(os.path.normpath(home + "/.prompt/mods/")):
                 remcount += 1
                 insert_at: int = None
                 if char == "o":
-                    if (overwrite_set == True):
-                        print(f"Error loading module {file}, tried to set o when o was already set", file=sys.stderr)
+                    if (PS == 1 and overwrite_set):
+                        print(f"Error loading module {file}, tried to set o for PS1 when o was already set for PS1", file=sys.stderr)
+                        cannot_load = True
+                        break
+                    if (PS == 2 and overwrite_2_set):
+                        print(f"Error loading module {file}, tried to set o for PS2 when o was already set for PS2", file=sys.stderr)
                         cannot_load = True
                         break
                     insert_at = 0
-                    overwrite_set = True
+                    if PS == 1: overwrite_set = True
+                    elif PS == 2: overwrite_2_set = True
                     flag_overwrite = True
                     continue
                 elif char == "m":
@@ -48,16 +62,34 @@ for root, dirs, files in os.walk(os.path.normpath(home + "/.prompt/mods/")):
             if (cannot_load): continue
             if (priority is None) or (insert_at is not None):
                 if (insert_at is None):
-                    nosort_modules.append({"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path})
+                    nosort_modules.append({"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path, "PS": PS})
                     continue
-                nosort_modules.insert(insert_at, {"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path})
+                if (overwrite_set):
+                    overwrite_module = {"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path, "PS": PS}
+                    continue
+                nosort_modules.insert(insert_at, {"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path, "PS": PS})
                 continue
             if not moddef.isalnum():
                 print(f"Error loading module {path}, priority is not a number", file=sys.stderr)
                 continue
             priority = int(moddef)
-            modules.append({"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path, "priority": priority})
+            modules.append({"manual": flag_manual, "stderr": flag_stderr, "overwrite": flag_overwrite, "path": path, "priority": priority, "PS": PS})
 modules.sort(key=lambda m: m["priority"])
+entry: str = ""
+if (overwrite_module is not None):
+    # Build script line
+    if (overwrite_module["manual"]):
+        entry = "source " + overwrite_module["path"]
+        print(entry)
+    else:
+        entry += f'export PS{overwrite_module["PS"]}="\\$(source {overwrite_module["path"]}'
+        if (overwrite_module["stderr"]): entry += " 2>&1"
+        entry += ')'
+        if (not overwrite_module["overwrite"]):
+            entry += f"$PS{overwrite_module["PS"]}"
+        entry += '"'
+        print(entry)
+
 for mod in nosort_modules:
     entry: str = ""
     # Build script line
@@ -65,11 +97,11 @@ for mod in nosort_modules:
         entry = "source " + mod["path"]
         print(entry)
         continue
-    entry += f'export PS1="\\$(source {mod["path"]}'
+    entry += f'export PS{mod["PS"]}="\\$(source {mod["path"]}'
     if (mod["stderr"]): entry += " 2>&1"
     entry += ')'
     if (not mod["overwrite"]):
-        entry += "$PS1"
+        entry += f"$PS{mod["PS"]}"
     entry += '"'
     print(entry)
 for mod in modules:
@@ -79,7 +111,7 @@ for mod in modules:
         entry = "source " + mod["path"]
         print(entry)
         continue
-    entry += f'export PS1="\\$(source {mod["path"]}'
+    entry += f'export PS{mod["PS"]}="\\$(source {mod["path"]}'
     if (mod["stderr"]): entry += " 2>&1"
-    entry += ')$PS1"'
+    entry += f')$PS{mod["PS"]}"'
     print(entry)
