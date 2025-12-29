1456634442abbb68c5a11099d6434d005cf3987e
Tyopo131 <samecob62@gmail.com>
2025-12-29 12:05:57 +0000

    Change order of flags in flags.py to spell out "dome" (just for fun)

--- src/flags.py
+++ src/flags.py
@@ -1,8 +1,8 @@
 BASE = {
-    "o": lambda mod: setattr(s, "overwrite", True),
     "d": lambda mod: setattr(s, "dry_run", True),
-    "e": lambda mod: setattr(s, "stderr", True),
-    "m": lambda mod: setattr(s, "manual", True)
+    "o": lambda mod: setattr(s, "overwrite", True),
+    "m": lambda mod: setattr(s, "manual", True),
+    "e": lambda mod: setattr(s, "stderr", True)
 }
 PS1 = BASE.copy()
 PS2 = BASE.copy()
